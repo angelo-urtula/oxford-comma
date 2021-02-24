0a8c6c4c79b73809ed6a1a7b051d4ad2d7d20513
angelo <aurtula1@gmail.com>
2021-02-24 15:11:03 -0500

    commit tree 066c158f6e201abe5a2d7234779c26f7de7a5204

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    if array.size == 1
+        return array.join
+    elsif array.size == 2
+        return array.join(" and ")
+    elsif array.size == 3
+        return array.join(", ").insert(14, "and ")
+    elsif array.size == 5
+        return array.join(", ").insert(-15, " and")
+    elsif array.size == 7
+        return array.join(", ").insert(-9, " and")
+end
 end
